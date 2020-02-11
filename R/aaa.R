4fb0c1010e2abfd1a8c0d528203ae4fd198aae8a
Rainer M. Krug <Rainer@krugs.de>
2020-02-11 14:08:22 +0100

    v0.5.1: add openssl to suggests and fix warnings concerning global variables

--- R/aaa.R
+++ R/aaa.R
@@ -6,3 +6,19 @@
   )
   options(LEEF.measurement.flowcam = opt)
 }
+
+utils::globalVariables(
+	c(
+		".", "Area (ABD)", "Area (Filled)", "Aspect Ratio", "Average Blue", 
+		"Average Green", "Average Red", "Circle Fit",
+		"Circularity", "Circularity (Hu)", "Class", "Compactness",
+		"Convex Perimeter", "Convexity", "Date", "Diameter (ABD)",
+		"Diameter (ESD)", "Edge Gradient", "Elongation", "Feret Angle Max", 
+		"Feret Angle Min", "Fiber Curl", "Fiber Straightness",
+		"Geodesic Aspect Ratio", "Geodesic Length", "Geodesic Thickness", 
+		"ID", "Image File", "Intensity", "Length", "Particle ID", "Perimeter", 
+		"Ratio Blue/Green", "Ratio Red/Blue", "Ratio Red/Green", "Roughness", 
+		"Sigma Intensity", "Sum Intensity", "Symmetry", "Transparency", 
+		"Volume (ABD)", "Volume (ESD)", "Width"
+	)
+)
