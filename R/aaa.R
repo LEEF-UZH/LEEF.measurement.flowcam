0548c71cdfd3873918835a3cbb6f1b4eb453d7ca
Rainer M. Krug <Rainer@krugs.de>
2020-11-12 13:42:29 +0100

    Add `par_...()` infrastructure for parameter usage

--- R/aaa.R
+++ R/aaa.R
@@ -1,4 +1,6 @@
+# Create cache environment ------------------------------------------------
 
+.FLOWCAM_CACHE <- new.env(FALSE, parent = globalenv())
 
 .onLoad <- function(lib, pkg) {
   opt <-  list(
@@ -9,16 +11,16 @@
 
 utils::globalVariables(
 	c(
-		".", "Area (ABD)", "Area (Filled)", "Aspect Ratio", "Average Blue", 
+		".", "Area (ABD)", "Area (Filled)", "Aspect Ratio", "Average Blue",
 		"Average Green", "Average Red", "Circle Fit",
 		"Circularity", "Circularity (Hu)", "Class", "Compactness",
 		"Convex Perimeter", "Convexity", "Date", "Diameter (ABD)",
-		"Diameter (ESD)", "Edge Gradient", "Elongation", "Feret Angle Max", 
+		"Diameter (ESD)", "Edge Gradient", "Elongation", "Feret Angle Max",
 		"Feret Angle Min", "Fiber Curl", "Fiber Straightness",
-		"Geodesic Aspect Ratio", "Geodesic Length", "Geodesic Thickness", 
-		"ID", "Image File", "Intensity", "Length", "Particle ID", "Perimeter", 
-		"Ratio Blue/Green", "Ratio Red/Blue", "Ratio Red/Green", "Roughness", 
-		"Sigma Intensity", "Sum Intensity", "Symmetry", "Transparency", 
+		"Geodesic Aspect Ratio", "Geodesic Length", "Geodesic Thickness",
+		"ID", "Image File", "Intensity", "Length", "Particle ID", "Perimeter",
+		"Ratio Blue/Green", "Ratio Red/Blue", "Ratio Red/Green", "Roughness",
+		"Sigma Intensity", "Sum Intensity", "Symmetry", "Transparency",
 		"Volume (ABD)", "Volume (ESD)", "Width"
 	)
 )
