3d422814bacbe6f3836457bd38ecd0855f72b382
Rainer M. Krug <Rainer@krugs.de>
2021-12-15 15:49:34 +0100

    0.8.28: Moved classification into separate function `classify()`

---
+++ R/classify.R
@@ -0,0 +1,124 @@
+#' Classify `algae_traits` nad calculates densities
+#'
+#' @param algae_traits algae_traits
+#' @param classifiers_constant constant tempersture classifier
+#' @param classifiers_increasing increasing temperature classifier
+#' @param composition composition
+#' @param species_tracked species tracked
+#'
+#' @return `list` containing two objects:
+#'    - `algae_traits` including species
+#'    - `algae_densities` densities of the differenc particles identifieds
+#' @export
+#'
+#' @md
+#'
+#' @examples
+classify <- function(
+  algae_traits,
+  classifiers_constant,
+  classifiers_increasing,
+  composition,
+  species_tracked
+){
+  # 2. Make a list of 32 dataframes: split morph_mvt based on species combination and temperature regime
+
+  algae_traits_list <- split(x = algae_traits,
+                             f = algae_traits$bottle,
+                             drop = TRUE)
+
+  # 3. Predict species identities in the 32 dfs based on the 32 rf classifiers
+
+  for (i in seq_along(algae_traits_list)) {
+
+    df <- algae_traits_list[[i]]
+
+    temperature_treatment <- unique(df$temperature) # either "constant" or "increasing"
+    composition_id <- unique(df$composition) # a char between c_01 and c_16
+    noNAs <- !rowSums(is.na(df)) > 0
+
+    if (temperature_treatment == "constant") {
+      pr <- predict(classifiers_constant[[composition_id]], df, probability = TRUE)
+      df$species[noNAs] <- as.character(pr) # species prediction
+      df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max) # probability of each species prediction
+      probabilities <- attributes(pr)$probabilities
+      colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
+      df <- cbind(df, probabilities)
+    } else {
+      pr <- predict(classifiers_increasing[[composition_id]], df, probability = TRUE)
+      df$species[noNAs] <- as.character(pr) # species prediction
+      df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max)  # probability of each species prediction
+      probabilities <- attributes(pr)$probabilities
+      colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
+      df <- cbind(df, probabilities)
+    }
+    algae_traits_list[[i]] <- df
+  }
+
+  # 4. Merge the 32 dfs back into a single df: algae_traits
+
+  algae_traits <- purrr::reduce(algae_traits_list, dplyr::full_join)
+
+  #############################################################
+  # calculate species densities -------------------------------------------------------------------------
+  #############################################################
+
+  algae_density <- algae_traits %>%
+    group_by(
+      Date_Flowcam,
+      species,
+      bottle,
+      composition,
+      temperature,
+      incubator,
+      volume_imaged,
+      dilution_factor,
+      richness
+    ) %>%
+    summarise(count = n()) %>%
+    mutate(density = count * dilution_factor / volume_imaged)
+
+
+  # -----------------------------------------------------------------------------------------------------
+  # add density = 0 for extinct species ------------------------------------------------------------
+
+
+  comp_id <- unique(composition$composition)
+  composition <- composition %>%
+    dplyr::select(tidyselect::any_of(species_tracked))
+
+  composition.list <- apply(composition, 1, function(x) {
+    idx <- which(x == 1)
+    names(idx)
+  })
+  names(composition.list) <- comp_id
+
+  algae_density_list <- split(x = algae_density,
+                              f = algae_density$bottle,
+                              drop = T)
+
+  for (i in seq_along(algae_density_list)) {
+    df <- algae_density_list[[i]]
+    ID <- unique(df$composition)
+    idx <- which(!is.element(unlist(composition.list[ID]), df$species))
+    if (length(idx) == 0) next
+    for (j in idx) {
+      new.entry <- tail(df, 1)
+      new.entry$species <- composition.list[[ID]][j]
+      new.entry$density <- 0
+      new.entry$count <- 0
+      df <- rbind(df, new.entry)
+    }
+    algae_density_list[[i]] <- df
+  }
+
+  algae_density <- do.call("rbind", algae_density_list) %>%
+    filter(species %in% species_tracked)
+
+  return(
+    list(
+      algae_traits = algae_traits,
+      algae_density = algae_density
+    )
+  )
+}
--- R/extractor_flowcam_classify.R
+++ R/extractor_flowcam_classify.R
@@ -62,7 +62,7 @@ extractor_flowcam_classify <- function(input, output) {
 #############################################################
 
 design <- read.csv(design_file)
-comps <- read.csv(composition_file)
+# comps <- read.csv(composition_file)
 
 algae_traits <- readRDS(algae_traits_file)
 
@@ -85,108 +85,122 @@ algae_traits <- readRDS(algae_traits_file)
 
 algae_traits <- dplyr::left_join(algae_traits, design, "bottle")
 
-# 1. Load in random classifiers (rds)
 
-#classifier files
+result <- LEEF.measurement.flowcam::classify(
+  algae_traits = algae_traits,
+  classifiers_constant = readRDS(name_constant),
+  classifiers_increasing = readRDS(name_increasing),
+  composition = read.csv(composition_file),
+  species_tracked = par_species_tracked()
+)
 
+algae_traits <- result$algae_traits
+algae_density <- result$algae_density
 
-classifiers_constant <- readRDS(name_constant)
-classifiers_increasing <- readRDS(name_increasing)
-
-
-# 2. Make a list of 32 dataframes: split morph_mvt based on species combination and temperature regime
-
-algae_traits_list <- split(x = algae_traits,
-                           f = algae_traits$bottle,
-                           drop = TRUE)
-
-# 3. Predict species identities in the 32 dfs based on the 32 rf classifiers
-
-for (i in seq_along(algae_traits_list)) {
-
-  df <- algae_traits_list[[i]]
-
-  temperature_treatment <- unique(df$temperature) # either "constant" or "increasing"
-  composition_id <- unique(df$composition) # a char between c_01 and c_16
-  noNAs <- !rowSums(is.na(df)) > 0
-
-  if (temperature_treatment == "constant") {
-    pr <- predict(classifiers_constant[[composition_id]], df, probability = TRUE)
-    df$species[noNAs] <- as.character(pr) # species prediction
-    df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max) # probability of each species prediction
-    probabilities <- attributes(pr)$probabilities
-    colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
-    df <- cbind(df, probabilities)
-  } else {
-    pr <- predict(classifiers_increasing[[composition_id]], df, probability = TRUE)
-    df$species[noNAs] <- as.character(pr) # species prediction
-    df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max)  # probability of each species prediction
-    probabilities <- attributes(pr)$probabilities
-    colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
-    df <- cbind(df, probabilities)
-  }
-  algae_traits_list[[i]] <- df
-}
-
-# 4. Merge the 32 dfs back into a single df: algae_traits
-
-algae_traits <- purrr::reduce(algae_traits_list, dplyr::full_join)
-
-#############################################################
-# calculate species densities -------------------------------------------------------------------------
-#############################################################
-
-algae_density <- algae_traits %>%
-  group_by(
-    Date_Flowcam,
-    species,
-    bottle,
-    composition,
-    temperature,
-    incubator,
-    volume_imaged,
-    dilution_factor,
-    richness
-  ) %>%
-  summarise(count = n()) %>%
-  mutate(density = count * dilution_factor / volume_imaged)
-
-
-# -----------------------------------------------------------------------------------------------------
-# add density = 0 for extinct species ------------------------------------------------------------
-
-
-comp_id <- unique(comps$composition)
-comps <- comps %>%
-  dplyr::select(tidyselect::any_of(par_species_tracked()))
-
-comps.list <- apply(comps, 1, function(x) {
-  idx <- which(x == 1)
-  names(idx)
-})
-names(comps.list) <- comp_id
-
-algae_density_list <- split(x = algae_density,
-                            f = algae_density$bottle,
-                            drop = T)
-
-for (i in seq_along(algae_density_list)) {
-  df <- algae_density_list[[i]]
-  ID <- unique(df$composition)
-  idx <- which(!is.element(unlist(comps.list[ID]), df$species))
-  if (length(idx) == 0) next
-  for (j in idx) {
-    new.entry <- tail(df, 1)
-    new.entry$species <- comps.list[[ID]][j]
-    new.entry$density <- 0
-    new.entry$count <- 0
-    df <- rbind(df, new.entry)
-  }
-  algae_density_list[[i]] <- df
-}
-
-algae_density <- do.call("rbind", algae_density_list) %>%
-  filter(species %in% par_species_tracked())
+###
+### This has been re-located into the function `classify`
+###
+# # 1. Load in random classifiers (rds)
+#
+# #classifier files
+#
+#
+# classifiers_constant <- readRDS(name_constant)
+# classifiers_increasing <- readRDS(name_increasing)
+#
+# # 2. Make a list of 32 dataframes: split morph_mvt based on species combination and temperature regime
+#
+# algae_traits_list <- split(x = algae_traits,
+#                            f = algae_traits$bottle,
+#                            drop = TRUE)
+#
+# # 3. Predict species identities in the 32 dfs based on the 32 rf classifiers
+#
+# for (i in seq_along(algae_traits_list)) {
+#
+#   df <- algae_traits_list[[i]]
+#
+#   temperature_treatment <- unique(df$temperature) # either "constant" or "increasing"
+#   composition_id <- unique(df$composition) # a char between c_01 and c_16
+#   noNAs <- !rowSums(is.na(df)) > 0
+#
+#   if (temperature_treatment == "constant") {
+#     pr <- predict(classifiers_constant[[composition_id]], df, probability = TRUE)
+#     df$species[noNAs] <- as.character(pr) # species prediction
+#     df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max) # probability of each species prediction
+#     probabilities <- attributes(pr)$probabilities
+#     colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
+#     df <- cbind(df, probabilities)
+#   } else {
+#     pr <- predict(classifiers_increasing[[composition_id]], df, probability = TRUE)
+#     df$species[noNAs] <- as.character(pr) # species prediction
+#     df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max)  # probability of each species prediction
+#     probabilities <- attributes(pr)$probabilities
+#     colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
+#     df <- cbind(df, probabilities)
+#   }
+#   algae_traits_list[[i]] <- df
+# }
+#
+# # 4. Merge the 32 dfs back into a single df: algae_traits
+#
+# algae_traits <- purrr::reduce(algae_traits_list, dplyr::full_join)
+#
+# #############################################################
+# # calculate species densities -------------------------------------------------------------------------
+# #############################################################
+#
+# algae_density <- algae_traits %>%
+#   group_by(
+#     Date_Flowcam,
+#     species,
+#     bottle,
+#     composition,
+#     temperature,
+#     incubator,
+#     volume_imaged,
+#     dilution_factor,
+#     richness
+#   ) %>%
+#   summarise(count = n()) %>%
+#   mutate(density = count * dilution_factor / volume_imaged)
+#
+#
+# # -----------------------------------------------------------------------------------------------------
+# # add density = 0 for extinct species ------------------------------------------------------------
+#
+#
+# comp_id <- unique(comps$composition)
+# comps <- comps %>%
+#   dplyr::select(tidyselect::any_of(par_species_tracked()))
+#
+# comps.list <- apply(comps, 1, function(x) {
+#   idx <- which(x == 1)
+#   names(idx)
+# })
+# names(comps.list) <- comp_id
+#
+# algae_density_list <- split(x = algae_density,
+#                             f = algae_density$bottle,
+#                             drop = T)
+#
+# for (i in seq_along(algae_density_list)) {
+#   df <- algae_density_list[[i]]
+#   ID <- unique(df$composition)
+#   idx <- which(!is.element(unlist(comps.list[ID]), df$species))
+#   if (length(idx) == 0) next
+#   for (j in idx) {
+#     new.entry <- tail(df, 1)
+#     new.entry$species <- comps.list[[ID]][j]
+#     new.entry$density <- 0
+#     new.entry$count <- 0
+#     df <- rbind(df, new.entry)
+#   }
+#   algae_density_list[[i]] <- df
+# }
+#
+# algae_density <- do.call("rbind", algae_density_list) %>%
+#   filter(species %in% par_species_tracked())
 
 #############################################################
 ### >>> END SCRIPT ##########################################
