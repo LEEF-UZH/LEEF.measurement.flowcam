e739ec70d3316b531d7bbc14722091189bf94e10
Rainer M. Krug <Rainer@krugs.de>
2022-01-04 10:14:32 +0100

    Metadata and comment formating

--- R/extractor_flowcam_classify.R
+++ R/extractor_flowcam_classify.R
@@ -33,16 +33,16 @@ extractor_flowcam_classify <- function(input, output) {
   processing <- file.path(normalizePath(output), "flowcam", paste0("EXTRACTING_CLASSIFY.FLOWCAM", ".PROCESSING"))
   error <- file.path(normalizePath(output), "flowcam", paste0("ERROR.EXTRACTING_CLASSIFY.FLOWCAM", ".ERROR"))
   on.exit({
-      if (file.exists(processing)) {
-        unlink(processing)
-        file.create(error)
-      }
+    if (file.exists(processing)) {
+      unlink(processing)
+      file.create(error)
     }
+  }
   )
   file.create(processing)
   ##
 
-# Get flowcam directory names ------------------------------------------------------
+  # Get flowcam directory names ------------------------------------------------------
 
   flowcam_path <- file.path(input, "flowcam")
 
@@ -53,162 +53,161 @@ extractor_flowcam_classify <- function(input, output) {
   design_file <- file.path(flowcam_path, "experimental_design.csv")
   composition_file <- file.path(flowcam_path, "compositions.csv")
 
-# the classifiers for increasing temperatures will have to be updated during experiment!!
+  # the classifiers for increasing temperatures will have to be updated during experiment!!
   name_constant <- file.path(flowcam_path, par_classifier_constant())
   name_increasing <- file.path(flowcam_path, par_classifier_increasing())
 
-#############################################################
-### <<< BEGIN SCRIPT ########################################
-#############################################################
+  #############################################################
+  ### <<< BEGIN SCRIPT ########################################
+  #############################################################
 
-# design <- read.csv(design_file)
-# comps <- read.csv(composition_file)
+  # design <- read.csv(design_file)
+  # comps <- read.csv(composition_file)
 
-# algae_traits <- readRDS(algae_traits_file)
+  # algae_traits <- readRDS(algae_traits_file)
 
-#############################################################
-# Classification  ------------------------------------------------------------------------
-#############################################################
+  #############################################################
+  # Classification  ------------------------------------------------------------------------
+  #############################################################
 
-# next steps: classify each particle in algae_traits
-# then calculate density for each species (mutiply the count for each species with dilution_factor
-# and divide by volume_imaged)
-# in the end we want to keep the dataframe algae_traits (then containing a column with species identity)
-# the dataframe meta_data
-# and the dataframe that contains the density/ml for each species
-
-
-# Predict species identities with svm --------------------------------------------------------
-# -----------------------------------------------------------------------------------------------------
+  # next steps: classify each particle in algae_traits
+  # then calculate density for each species (mutiply the count for each species with dilution_factor
+  # and divide by volume_imaged)
+  # in the end we want to keep the dataframe algae_traits (then containing a column with species identity)
+  # the dataframe meta_data
+  # and the dataframe that contains the density/ml for each species
 
 
-result <- LEEF.measurement.flowcam::classify(
-  algae_traits = readRDS(algae_traits_file),
-  classifiers_constant = readRDS(name_constant),
-  classifiers_increasing = readRDS(name_increasing),
-  composition = read.csv(composition_file),
-  exp_design = read.csv(design_file),
-  species_tracked = par_species_tracked(),
-  timestamp = yaml::read_yaml(file.path(input,  "flowcam", "sample_metadata.yml"))$timestamp
+  # Predict species identities with svm --------------------------------------------------------
+  # -----------------------------------------------------------------------------------------------------
 
-)
+  # algae_traits <- result$algae_traits
+  # algae_density <- result$algae_density
 
-# algae_traits <- result$algae_traits
-# algae_density <- result$algae_density
-
-###
-### This has been re-located into the function `classify`
-###
-# # 1. Load in random classifiers (rds)
-#
-# #classifier files
-#
-#
-# classifiers_constant <- readRDS(name_constant)
-# classifiers_increasing <- readRDS(name_increasing)
-#
-# # 2. Make a list of 32 dataframes: split morph_mvt based on species combination and temperature regime
-#
-# algae_traits_list <- split(x = algae_traits,
-#                            f = algae_traits$bottle,
-#                            drop = TRUE)
-#
-# # 3. Predict species identities in the 32 dfs based on the 32 rf classifiers
-#
-# for (i in seq_along(algae_traits_list)) {
-#
-#   df <- algae_traits_list[[i]]
-#
-#   temperature_treatment <- unique(df$temperature) # either "constant" or "increasing"
-#   composition_id <- unique(df$composition) # a char between c_01 and c_16
-#   noNAs <- !rowSums(is.na(df)) > 0
-#
-#   if (temperature_treatment == "constant") {
-#     pr <- predict(classifiers_constant[[composition_id]], df, probability = TRUE)
-#     df$species[noNAs] <- as.character(pr) # species prediction
-#     df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max) # probability of each species prediction
-#     probabilities <- attributes(pr)$probabilities
-#     colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
-#     df <- cbind(df, probabilities)
-#   } else {
-#     pr <- predict(classifiers_increasing[[composition_id]], df, probability = TRUE)
-#     df$species[noNAs] <- as.character(pr) # species prediction
-#     df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max)  # probability of each species prediction
-#     probabilities <- attributes(pr)$probabilities
-#     colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
-#     df <- cbind(df, probabilities)
-#   }
-#   algae_traits_list[[i]] <- df
-# }
-#
-# # 4. Merge the 32 dfs back into a single df: algae_traits
-#
-# algae_traits <- purrr::reduce(algae_traits_list, dplyr::full_join)
-#
-# #############################################################
-# # calculate species densities -------------------------------------------------------------------------
-# #############################################################
-#
-# algae_density <- algae_traits %>%
-#   group_by(
-#     Date_Flowcam,
-#     species,
-#     bottle,
-#     composition,
-#     temperature,
-#     incubator,
-#     volume_imaged,
-#     dilution_factor,
-#     richness
-#   ) %>%
-#   summarise(count = n()) %>%
-#   mutate(density = count * dilution_factor / volume_imaged)
-#
-#
-# # -----------------------------------------------------------------------------------------------------
-# # add density = 0 for extinct species ------------------------------------------------------------
-#
-#
-# comp_id <- unique(comps$composition)
-# comps <- comps %>%
-#   dplyr::select(tidyselect::any_of(par_species_tracked()))
-#
-# comps.list <- apply(comps, 1, function(x) {
-#   idx <- which(x == 1)
-#   names(idx)
-# })
-# names(comps.list) <- comp_id
-#
-# algae_density_list <- split(x = algae_density,
-#                             f = algae_density$bottle,
-#                             drop = T)
-#
-# for (i in seq_along(algae_density_list)) {
-#   df <- algae_density_list[[i]]
-#   ID <- unique(df$composition)
-#   idx <- which(!is.element(unlist(comps.list[ID]), df$species))
-#   if (length(idx) == 0) next
-#   for (j in idx) {
-#     new.entry <- tail(df, 1)
-#     new.entry$species <- comps.list[[ID]][j]
-#     new.entry$density <- 0
-#     new.entry$count <- 0
-#     df <- rbind(df, new.entry)
-#   }
-#   algae_density_list[[i]] <- df
-# }
-#
-# algae_density <- do.call("rbind", algae_density_list) %>%
-#   filter(species %in% par_species_tracked())
+  ###
+  ### This has been re-located into the function `classify`
+  ###
+  # # 1. Load in random classifiers (rds)
+  #
+  # #classifier files
+  #
+  #
+  # classifiers_constant <- readRDS(name_constant)
+  # classifiers_increasing <- readRDS(name_increasing)
+  #
+  # # 2. Make a list of 32 dataframes: split morph_mvt based on species combination and temperature regime
+  #
+  # algae_traits_list <- split(x = algae_traits,
+  #                            f = algae_traits$bottle,
+  #                            drop = TRUE)
+  #
+  # # 3. Predict species identities in the 32 dfs based on the 32 rf classifiers
+  #
+  # for (i in seq_along(algae_traits_list)) {
+  #
+  #   df <- algae_traits_list[[i]]
+  #
+  #   temperature_treatment <- unique(df$temperature) # either "constant" or "increasing"
+  #   composition_id <- unique(df$composition) # a char between c_01 and c_16
+  #   noNAs <- !rowSums(is.na(df)) > 0
+  #
+  #   if (temperature_treatment == "constant") {
+  #     pr <- predict(classifiers_constant[[composition_id]], df, probability = TRUE)
+  #     df$species[noNAs] <- as.character(pr) # species prediction
+  #     df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max) # probability of each species prediction
+  #     probabilities <- attributes(pr)$probabilities
+  #     colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
+  #     df <- cbind(df, probabilities)
+  #   } else {
+  #     pr <- predict(classifiers_increasing[[composition_id]], df, probability = TRUE)
+  #     df$species[noNAs] <- as.character(pr) # species prediction
+  #     df$species_probability[noNAs] <- apply(attributes(pr)$probabilities, 1, max)  # probability of each species prediction
+  #     probabilities <- attributes(pr)$probabilities
+  #     colnames(probabilities) <- paste0(colnames(probabilities),"_prob")
+  #     df <- cbind(df, probabilities)
+  #   }
+  #   algae_traits_list[[i]] <- df
+  # }
+  #
+  # # 4. Merge the 32 dfs back into a single df: algae_traits
+  #
+  # algae_traits <- purrr::reduce(algae_traits_list, dplyr::full_join)
+  #
+  # #############################################################
+  # # calculate species densities -------------------------------------------------------------------------
+  # #############################################################
+  #
+  # algae_density <- algae_traits %>%
+  #   group_by(
+  #     Date_Flowcam,
+  #     species,
+  #     bottle,
+  #     composition,
+  #     temperature,
+  #     incubator,
+  #     volume_imaged,
+  #     dilution_factor,
+  #     richness
+  #   ) %>%
+  #   summarise(count = n()) %>%
+  #   mutate(density = count * dilution_factor / volume_imaged)
+  #
+  #
+  # # -----------------------------------------------------------------------------------------------------
+  # # add density = 0 for extinct species ------------------------------------------------------------
+  #
+  #
+  # comp_id <- unique(comps$composition)
+  # comps <- comps %>%
+  #   dplyr::select(tidyselect::any_of(par_species_tracked()))
+  #
+  # comps.list <- apply(comps, 1, function(x) {
+  #   idx <- which(x == 1)
+  #   names(idx)
+  # })
+  # names(comps.list) <- comp_id
+  #
+  # algae_density_list <- split(x = algae_density,
+  #                             f = algae_density$bottle,
+  #                             drop = T)
+  #
+  # for (i in seq_along(algae_density_list)) {
+  #   df <- algae_density_list[[i]]
+  #   ID <- unique(df$composition)
+  #   idx <- which(!is.element(unlist(comps.list[ID]), df$species))
+  #   if (length(idx) == 0) next
+  #   for (j in idx) {
+  #     new.entry <- tail(df, 1)
+  #     new.entry$species <- comps.list[[ID]][j]
+  #     new.entry$density <- 0
+  #     new.entry$count <- 0
+  #     df <- rbind(df, new.entry)
+  #   }
+  #   algae_density_list[[i]] <- df
+  # }
+  #
+  # algae_density <- do.call("rbind", algae_density_list) %>%
+  #   filter(species %in% par_species_tracked())
 
   # result$algae_traits$timestamp <- timestamp
   # result$algae_density$timestamp <- timestamp
 
-#############################################################
-### >>> END SCRIPT ##########################################
-#############################################################
+  result <- LEEF.measurement.flowcam::classify(
+    algae_traits = readRDS(algae_traits_file),
+    classifiers_constant = readRDS(name_constant),
+    classifiers_increasing = readRDS(name_increasing),
+    composition = read.csv(composition_file),
+    exp_design = read.csv(design_file),
+    species_tracked = par_species_tracked(),
+    timestamp = yaml::read_yaml(file.path(input,  "flowcam", "sample_metadata.yml"))$timestamp
 
-# SAVE --------------------------------------------------------------------
+  )
+
+  #############################################################
+  ### >>> END SCRIPT ##########################################
+  #############################################################
+
+  # SAVE --------------------------------------------------------------------
 
   #
   utils::write.csv(
@@ -237,7 +236,7 @@ result <- LEEF.measurement.flowcam::classify(
     to = file.path(output, "flowcam", "")
   )
 
-# Finalize ----------------------------------------------------------------
+  # Finalize ----------------------------------------------------------------
 
   unlink(processing)
   message("   done")
